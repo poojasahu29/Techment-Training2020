d28c5baf647a944b051829b278eef7f3ef5af9a1
pooja sahu <poojasahu29041996@gmail.com>
2020-05-29 14:13:34 +0530

    about Eclipse and program on how to work  breakpoint and debug the breakpoints

---
+++ day3_Assignments/src/com/techment/Forloop.java
@@ -0,0 +1,17 @@
+package com.techment;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1;i<=5;i++)
+		{
+			sum=sum+i;
+			System.out.println("i "+ i +" sum "+ sum);
+			
+		}
+		System.out.println("total sum "+ sum);
+	}
+
+}
