b0c4635535b3a5ae6e86be518e1606f0b00f9897
pooja sahu <poojasahu29041996@gmail.com>
2020-06-01 22:14:15 +0530

    upload Ex-11

---
+++ logic buildings programs/src/com/techment/Ex11_Pattern.java
@@ -0,0 +1,28 @@
+package com.techment;
+
+public class Ex11_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		        int i, j, n = 5; 
+		    
+		        for(i=5; i>=1; i--) // outer loop 
+		        {
+		           
+		            for(j=1; j<=i; j++) // inner loop 
+		            { 
+		                // printing j with a space  
+		                System.out.print(j+ " "); 
+		  
+		            } 
+		    
+		            // ending line after each row 
+		            System.out.println(); 
+		        } 
+		    } 
+		      
+		}
+
+	
+
+
