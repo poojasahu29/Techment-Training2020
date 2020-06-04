346efdf8b1ef4fa645a26b460504881f420932a2
pooja sahu <poojasahu29041996@gmail.com>
2020-06-04 17:48:34 +0530

    logic buildings programs

--- logic buildings programs/src/com/techment/Ex12_Pattern.java
+++ logic buildings programs/src/com/techment/Ex12_Pattern.java
@@ -6,21 +6,21 @@ public class Ex12_Pattern {
 		// TODO Auto-generated method stub
 		int count=1,i,j;
 		Scanner sc=new Scanner(System.in); 
-		System.out.print("Enter the numbers of lines you want");
+		System.out.println("Enter the numbers of lines you want");
 		int number=sc.nextInt();								//taking number of lines as input
 		System.out.println(" ");
-		System.out.print("pattern is:\n");
+		System.out.println("pattern is:\n");
 		
 		
 		for(i=number; i>=1; i-- ){						//outer for loop(parent)
 		 for(j=1; j<=i*2; j++){
-		 System.out.print(" ");					//create space from at begining of each row
+		 System.out.println(" ");					//create space from at begining of each row
 		 }											//inner for loop 1(child)
 		 for(j=i; j<=number; j++){					//print right part of Piramid
-		 System.out.print(j+" ");
+		 System.out.println(j+" ");
 		 }											//inner for loop 2
 		for(j=number-1; j>=i; j--){
-		 System.out.print(j+" "); 					//print left part of Piramid
+		 System.out.println(j+" "); 					//print left part of Piramid
 		 }											//inner for loop 3
 		  System.out.println();
 		count++;									//count increse with 1
