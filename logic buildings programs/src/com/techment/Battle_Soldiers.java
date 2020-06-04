59a6bce787b49384339d16445083aa36b5d75cc3
pooja sahu <poojasahu29041996@gmail.com>
2020-06-04 14:01:10 +0530

    logic buildings programs

---
+++ logic buildings programs/src/com/techment/Battle_Soldiers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Battle_Soldiers{
+	public static void main(String[] args) {
+		
+		int firstSoldier_number, lastSoldier_number;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter1st soldier number:");
+		firstSoldierNumber = scanner.nextInt();
+		System.out.print("Enter 1st soldier number:");
+		lastSoldierNumber =  scanner.nextInt();
+
+		System.out.println("Soldiers come forward: - ");
+
+		for (int i = firstSoldier_number; i<=lastSoldier_number ; i++)
+			System.out.println(i);
+	}
+}
