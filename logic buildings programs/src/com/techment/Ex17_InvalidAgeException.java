1bc4b2ce6c861d21f436438f8b393c62701b8afe
pooja sahu <poojasahu29041996@gmail.com>
2020-06-04 19:16:52 +0530

    logic buildings programs

--- logic buildings programs/src/com/techment/Ex17_InvalidAgeException.java
+++ logic buildings programs/src/com/techment/Ex17_InvalidAgeException.java
@@ -1,28 +1,30 @@
 package com.techment;
 import java.util.Scanner;
-public class Ex17_InvalidAgeException extends Exception {
-
-	public Ex17_InvalidAgeException(String str) {
-	  System.out.println(str);
-	 }
+public class Ex17_InvalidAgeException {
 	
 	 public static void main(String[] args) {
 	  Scanner s = new Scanner(System.in);
 	  System.out.print("Enter your age: ");
 	  int age = s.nextInt();
 	  
-	  try {
-	   if(18>=age) 
-	    throw new Ex17_InvalidAgeException("Invalid age");
-	   else
-	    System.out.println("Welcome to Vote....");
-	  }
-	  catch (Ex17_InvalidAgeException b) {
-	   System.out.println(b);
-	  }
+		try{
+			if (age >= 18) 
+				System.out.println("Welcome to Vote");
+			else
+				throw new InvalidAgeException("Invalid Age...");
+		}
+		catch(InvalidAgeException ex)
+		{
+			System.out.println(ex);
+		}
 	 }
 	
 
+	}
+class InvalidAgeException extends Exception{
+		public InvalidAgeException(String str){
+			super(str);
+		}
 	}
 
 
